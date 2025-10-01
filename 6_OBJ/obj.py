ed64050f5f17bd9d716243295062e676ec9f504d
git-take777 <git.coding.life@gmail.com>
2025-10-01 22:14:00 +0900

    feat: PythonのObjの仕組みを実装

---
+++ 6_OBJ/obj.py
@@ -0,0 +1,9 @@
+class Person:
+#   contructor
+  def __init__(self, name, gender, age):
+    self.name = name
+    self.age = age
+    self.gender = gender
+
+john = Person("John", 28, 'male')
+taro = Person("Taro", 30, 'male')
