8b834e608c0e6bc3c5d39e409d243773d82ee90c
git-take777 <git.coding.life@gmail.com>
2025-10-05 07:51:04 +0900

    feat: @propertyを使って getterとsetterを実装

--- 6_OBJ/getter_setter.py
+++ 6_OBJ/getter_setter.py
@@ -4,16 +4,19 @@ class Person:
         self.name = name
         # self.age = age
         self._age = age
-    def get_age(self):
+        # @propertyを使うこともできる→get_ageをageに変更
+    @property
+    def age(self):
         print("get_age is called")
         return self._age
-    def set_age(self, age):
+    @age.setter
+    def age(self, age):
         print("set_age is called")
         if age < 0:
             print("Age cannot be negative")
         else:
             self._age = age
-    age = property(get_age, set_age)
+    # age = property(get_age, set_age)
 john = Person("John", -20)
 print(john.name)
 print(john.age)
